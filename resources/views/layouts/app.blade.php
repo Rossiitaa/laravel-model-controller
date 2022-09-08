e7a6ac8f6036dfd47725e6c270dca2d283608db1
Rossiitaa <rosario.scuotto@outlook.it>
2022-09-08 16:05:03 +0200

    result printed on the page in the cards

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel Model Controller | @yield('title')</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <script src="{{asset('js/app.js')}}" defer></script>
 </head>
-<body>
+<body class="container">
     @yield('content')
 </body>
 </html>
--- resources/views/movie.blade.php
+++ resources/views/movie.blade.php
@@ -2,23 +2,23 @@
 @section('title', 'Movie')
 
 @section('content')
-    <ul>
+    <ul class="row p-5 ">
         @forelse ($movies as $movie)
-            <li>
-                {{ $movie->title }}
-            </li>
-            <li>
-                {{ $movie->original_title }}
-            </li>
-            <li>
-                {{ $movie->nationality }}
-            </li>
-            <li>
-                {{ $movie->date }}
-            </li>
-            <li>
-                {{ $movie->vote }}
-            </li>
+        <div class="card col-3 p-3 m-4">
+            <div class="card-body">
+                <h2 class="card-title">{{ $movie->title }}</h2>
+                <h5 class="card-subtitle my-2">{{ $movie->original_title }}</h5>
+                <p class="card-text">
+                    Nationality: {{ $movie->nationality}}
+                </p>
+                <p class="card-text">
+                    Date: {{ $movie->date }}
+                </p>
+                <p class="card-text">
+                    Vote: {{ $movie->vote }}
+                </p>
+            </div>
+        </div>
         @empty
             
         @endforelse
